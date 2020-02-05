fc95a5526b66110bbce6fae0d4725bf0790d799f
Diego Vasquez <botellongo69@outlook.com>
2020-02-05 21:38:01 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,8 +12,8 @@ class Patient
   end
   
   def appointments
-    Appointment.all.select do |song|
-      song.patient == self
+    Appointment.all.select do |appointment|
+      appointment.patient == self
     end
   end
   
